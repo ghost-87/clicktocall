88e575853536d5887ce4e0c2c9e94d9342ae2782
ghost-87 <57pi73@gmail.com>
2020-07-03 12:41:20 +0530

    build added

---
+++ build/precache-manifest.b7b03ed4dfb71b8233a0558dee4c2f1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877341b86a315a6a3544697498d70f17",
+    "url": "/clicktocall/index.html"
+  },
+  {
+    "revision": "cfc7c6785d9e49288d0f",
+    "url": "/clicktocall/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe83a02f68cbc1c4dc94",
+    "url": "/clicktocall/static/js/2.18bd0235.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/clicktocall/static/js/2.18bd0235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc7c6785d9e49288d0f",
+    "url": "/clicktocall/static/js/main.bd7d7464.chunk.js"
+  },
+  {
+    "revision": "e618869c282e23c125d3",
+    "url": "/clicktocall/static/js/runtime-main.4695cffd.js"
+  },
+  {
+    "revision": "19143c0ffab88ae2a7111205e7e92569",
+    "url": "/clicktocall/static/media/Cancelbutton .19143c0f.png"
+  },
+  {
+    "revision": "0fd7213f8940651900a109c3cc4ec292",
+    "url": "/clicktocall/static/media/call.0fd7213f.png"
+  }
+]);
